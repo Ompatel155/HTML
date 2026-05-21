8707646ca2ac915224a7bcc9e3beb99e76f96a0c
Ompatel155 <ompatel7898803421@gmail.com>
2026-05-21 20:54:27 +0530

    new

---
+++ javaScript/jquery/jqueryindex.js
@@ -0,0 +1,13 @@
+// console.log($("h1"))
+// $("h1").css('color','pink')
+
+
+$("#fadeIn").click(()=>{
+    $("#container").fadeIn()
+})
+$("fadeOut").click(()=>{
+    $("#container").fadeOut()
+})
+$("#fadetaggle").click(()=>{
+    $("#container").fadeToggle()
+})
